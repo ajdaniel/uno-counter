2f0f750a618e6a8658812e3599225ea7c34d946e
adaniel <adaniel@uk.ibm.com>
2017-01-20 11:41:08 +0000

    Improve the last button border functionality

--- uno-counter.js
+++ uno-counter.js
@@ -6,7 +6,7 @@
 
     app.component('unoCounter', {
         templateUrl: './uno-counter.component.html',
-        controller: function () {
+        controller: function ($timeout) {
             this.model = model;
             this.advanced = false;
             this.icons = true;
@@ -15,20 +15,37 @@
                 return Math.floor(this.model.count / 4) + 1;
             };
             this._lastbutton = '';
+            this._cancelTimeout = function () {
+                if (this._timeout) {
+                    $timeout.cancel(this._timeout);
+                }
+            }
             this.fullDK = function () {
                 this.model.addDonkeyKong();
                 this.model.addFFA();
                 this.model.addFFA();
                 this.model.addFFA();
+                this._cancelTimeout();
                 this._lastbutton = 'fdk';
             };
             this.addDK = function () {
                 this.model.addDonkeyKong();
                 this._lastbutton = 'dk';
+                this._cancelTimeout();
+                var ctrl = this;
+                this._timeout = $timeout(function() {
+                    ctrl._lastbutton = '';
+                }, 3000);
             }
             this.addFFA = function () {
                 this.model.addFFA();
+                this._cancelTimeout();
                 this._lastbutton = 'ffa';
+            }
+            this.clearCount = function () {
+                this.model.clearCount();
+                this._cancelTimeout();
+                this._lastbutton = '';
             }
         }
     });
